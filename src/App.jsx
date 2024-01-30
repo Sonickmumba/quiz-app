c93d8d23afc4796227f464eaac334fe4acdddd2e
Sonick Mumba <kingellie.mumba@gmail.com>
2024-01-30 18:50:48 +0200

    Add reducer and fetch data using useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
+import { useEffect, useReducer } from 'react'
 import './App.css'
 import Header from './components/header/Header'
+import Main from './components/main/Main'
+
+
+const initialState = {
+  questions: [
+
+  ],
+
+  // 'loading', 'active', 'error', 'ready', 'finished', 'dataReceived'
+  status: "loading"
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return {...state, questions: action.payload, status: 'ready'}
+      
+    // case 'received'
+  
+    default:
+      throw new Error("Action unknown")
+  }
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+ 
+  useEffect (() => {
+    const fetchData = async () => {
+     try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({
+          type: "dataReceived",
+          payload: data
+        });
+     } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+     } 
+    }
+    fetchData();
+  },[])
 
   return (
       <div className='app'>
         <Header />
+        <Main>
+          <p>1/15</p>
+          <p>Question?</p>
+        </Main>
       </div>
   )
 }
