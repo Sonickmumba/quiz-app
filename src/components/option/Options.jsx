64f618e98f5d66663095c64b625b375a64308ebe
Sonick Mumba <kingellie.mumba@gmail.com>
2024-02-01 00:46:15 +0200

    implement the correct answer

--- src/components/option/Options.jsx
+++ src/components/option/Options.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
 // import React from 'react'
 
-const Options = ({question}) => {
+const Options = ({ question, dispatch, answer }) => {
   return (
     <div className="options">
-        {question.options.map((opt) => (
-          <button className="btn btn-option" key={opt}>
-            {opt}
-          </button>
-        ))}
-      </div>
-  )
-}
+      {question.options.map((opt, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${index === question.correctOption ? "correct" : "wrong"}`}
+          key={opt}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {opt}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default Options
+export default Options;
