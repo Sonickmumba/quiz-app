aef718143b0a5900af79ad371c9628ab77975b00
Sonick Mumba <kingellie.mumba@gmail.com>
2024-02-01 11:06:07 +0200

    correct answer

--- src/components/option/Options.jsx
+++ src/components/option/Options.jsx
@@ -2,11 +2,14 @@
 // import React from 'react'
 
 const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
       {question.options.map((opt, index) => (
         <button
-          className={`btn btn-option ${index === answer ? "answer" : ""} ${index === question.correctOption ? "correct" : "wrong"}`}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered ? index === question.correctOption ? "correct" : "wrong" : ""
+          }`}
           key={opt}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
